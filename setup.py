bf902058c31cbc203706e136f35a8395bf7d8be0
share424 <share424@gmail.com>
2024-03-10 12:26:29 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
+
+MIN_REQUIRED_TORCH_VERSION = "2.0.0"
+MIN_REQUIRED_TORCHVISION_VERSION = "0.15.0"
 REQUIREMENTS = [
-    'torch>=2.0.0',
-    'torchvision>=0.17.0',
     'numpy>=1.26.3',
     'pydantic>=2.6.3',
     'pyyaml>=6.0.1',
@@ -12,6 +13,34 @@ REQUIREMENTS = [
     'nltk>=3.8.1',
     'pycocoevalcap>=1.2.0',
 ]
+
+def check_if_torch_installed():
+    try:
+        import torch
+
+        current_version = torch.__version__
+
+        # remove any suffixes (e.g. +cu102 in the case of CUDA 10.2)
+        current_version = current_version.split("+")[0]
+
+        if torch.__version__ < MIN_REQUIRED_TORCH_VERSION:
+            REQUIREMENTS.append(f"torch>={MIN_REQUIRED_TORCH_VERSION}")
+    except ImportError:
+        REQUIREMENTS.append(f"torch>={MIN_REQUIRED_TORCH_VERSION}")
+
+def check_if_torchvision_installed():
+    try:
+        import torchvision
+
+        if torchvision.__version__ < MIN_REQUIRED_TORCHVISION_VERSION:
+            REQUIREMENTS.append(f"torchvision>={MIN_REQUIRED_TORCHVISION_VERSION}")
+    except ImportError:
+        REQUIREMENTS.append(f"torchvision>={MIN_REQUIRED_TORCHVISION_VERSION}")
+
+
+check_if_torch_installed()
+check_if_torchvision_installed()
+
 
 setup(
     name='image_captioning',
